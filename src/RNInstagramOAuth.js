8610bceffeaefa4539dc47fccc5b0dc2c270c2b7
Aakash Sigdel <aakash.sigdel@gmail.com>
2016-04-21 06:23:57 +0545

    remove RN as dependency; changed LinkingIOS to Linking

--- src/RNInstagramOAuth.js
+++ src/RNInstagramOAuth.js
@@ -2,12 +2,12 @@ var React = require('react-native');
 var shittyQs = require('shitty-qs');
 
 var {
-    LinkingIOS
+    Linking
     } = React;
 
 function RNInstagramOAuth (client_id, redirect_url, callback) {
 
-    LinkingIOS.addEventListener('url', handleUrl);
+    Linking.addEventListener('url', handleUrl);
 
     function handleUrl (event) {
         var [, query_string] = event.url.match(/\#(.*)/);
@@ -17,10 +17,10 @@ function RNInstagramOAuth (client_id, redirect_url, callback) {
         } else {
             callback(new Error('Oauth2 security error'));
         }
-        LinkingIOS.removeEventListener('url', handleUrl);
+        Linking.removeEventListener('url', handleUrl);
     }
 
-    LinkingIOS.openURL(
+    Linking.openURL(
         'https://instagram.com/oauth/authorize/?client_id='+client_id+
         '&redirect_uri='+redirect_url+
         '&response_type=token'+
